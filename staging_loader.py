0f230118d2952da18c44ce2531899de8a48c2e14
Alexander Mayr <alex.mayr.mobil@gmail.com>
2025-08-21 17:44:30 +0200

    cosmetic code cleanup and import optimization

--- staging_loader.py
+++ staging_loader.py
@@ -1,13 +1,13 @@
-import json
+from logger import log
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
 from pyspark.sql.types import StructType, StructField, StringType
-from logger import log
 
 from config import (
     KAFKA_BOOTSTRAP_SERVERS, KAFKA_TOPIC, KAFKA_STARTING_OFFSETS, KAFKA_GROUP_ID,
     SPARK_MASTER, SPARK_DRIVER_MEMORY, SPARK_EXECUTOR_MEMORY, SPARK_SQL_SHUFFLE_PARTITIONS, SPARK_ADAPTIVE_EXECUTION
 )
+
 
 def get_spark():
     log.info("Initializing Spark session for staging loader")
@@ -25,6 +25,7 @@ def get_spark():
     )
     spark.conf.set("spark.sql.adaptive.enabled", SPARK_ADAPTIVE_EXECUTION)
     return spark
+
 
 def get_kafka_stream(spark, table_name, schema):
     if schema is None:
@@ -47,8 +48,9 @@ def get_kafka_stream(spark, table_name, schema):
         .format("kafka")
         .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
         .option("subscribe", KAFKA_TOPIC)
-        .option("startingOffsets", KAFKA_STARTING_OFFSETS)
-        .option("groupIdPrefix", KAFKA_GROUP_ID)
+        .option("startingOffsets", "earliest")
+        .option("endingOffsets", "latest")
+        .option("failOnDataLoss", "false")
         .load()
     )
     df_json = df.select(from_json(col("value").cast("string"), json_schema).alias("json"))
